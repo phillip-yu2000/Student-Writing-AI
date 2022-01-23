ebfb0f1a2a99894aca1ba73ad43453c5fc6a33c7
Phillip Yu <69870888+phillip-yu2000@users.noreply.github.com>
2022-01-22 21:31:10 -0800

    packages and script to download model

---
+++ download_longformer.py
@@ -0,0 +1,32 @@
+import os 
+import torch
+from transformers import AutoTokenizer, AutoConfig, \
+    AutoModelForTokenClassification
+
+def main():
+    MODEL_NAME = 'allenai/longformer-base-4096'
+
+    # DOWNLOAD TOKENIZER, CONFIGURATION, AND MODEL
+    tokenizer = AutoTokenizer.from_pretrained(
+        MODEL_NAME, add_prefix_space = True
+    )
+    
+    config_model = AutoConfig.from_pretrained(
+        MODEL_NAME, num_labels = 15
+    )
+
+    model = AutoModelForTokenClassification.from_pretrained(
+        MODEL_NAME, num_labels = 15
+    )
+
+    # SAVE TOKENIZER, CONFIGURATION, AND MODEL
+    os.mkdir('longformer')
+    
+    tokenizer.save_pretrained('longformer')
+    
+    config_model.save_pretrained('longformer')
+    
+    model.save_pretrained('longformer')
+
+if __name__ == '__main__':
+    main()
